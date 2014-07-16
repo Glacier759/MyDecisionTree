ce43836025fe8f038559e64e75a3b52e6acc5578
Glacier <OurHom.759@gmail.com>
2014-07-16 11:59:39 +0800

    可以计算信息增益

--- src/com/DecisionTree/InformationGain.java
+++ src/com/DecisionTree/InformationGain.java
@@ -2,11 +2,17 @@
 package com.DecisionTree;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InformationGain {
 	
+	public Double Entropy_S = null;
 	public InformationGain() {
 		
+	}
+	
+	public InformationGain(ArrayList<Integer> CalcuParam) {
+		this.Entropy_S = this.getInformationEntropy(CalcuParam);
 	}
 	
 	public double getInformationEntropy( ArrayList<Integer> CalcuParam ) {
@@ -23,5 +29,26 @@ public class InformationGain {
 			TotalEntropy += SingleEntropy;
 		}
 		return TotalEntropy;
+	}
+	
+	public double getInformationGain( HashMap<Integer,Double> CalcuParam ) {
+		
+		if ( this.Entropy_S == null ) {
+			System.out.println("Entropy_S is null");
+			return 0;
+		}
+		
+		int 	TotalCount = 0;
+		double 	TotalGain = 0;
+		for ( int SingleCount:CalcuParam.keySet() ) {
+			TotalCount += SingleCount;
+		}
+		for ( Integer SingleCount:CalcuParam.keySet() ) {
+			double Proportion = (double)SingleCount/(double)TotalCount;
+			double SingleGain = -(Proportion)*CalcuParam.get(SingleCount);
+			TotalGain += SingleGain;
+		}
+		TotalGain = this.Entropy_S + TotalGain;
+		return TotalGain;
 	}
 }
--- src/com/DecisionTree/Main.java
+++ src/com/DecisionTree/Main.java
@@ -4,6 +4,7 @@ package com.DecisionTree;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -20,18 +21,35 @@ public class Main {
 		String Headers[] = TrainingHeader.split(",");
 		
 		InformationGain infoGain = new InformationGain();
-		ArrayList<Integer> Test = new ArrayList<Integer>();
-		Test.add(3);
-		Test.add(4);
-		System.out.println(infoGain.getInformationEntropy(Test));
+		//ArrayList<Integer> Test = new ArrayList<Integer>();
+		//Test.add(3);
+		//Test.add(4);
+		//System.out.println(infoGain.getInformationEntropy(Test));
 		
-		System.out.println(TrainingInfo.size());
+		ArrayList<Integer> Wind = new ArrayList<Integer>();
+		ArrayList<Integer> Weak = new ArrayList<Integer>();
+		ArrayList<Integer> Strong = new ArrayList<Integer>();
+		Wind.add(9);
+		Wind.add(5);
+		Weak.add(6);
+		Weak.add(2);
+		Strong.add(3);
+		Strong.add(3);
+		
+		HashMap<Integer, Double> GainTest = new HashMap<Integer, Double>();
+		GainTest.put(8, infoGain.getInformationEntropy(Weak));
+		GainTest.put(6, infoGain.getInformationEntropy(Strong));
+		
+		double Entropy_S = infoGain.getInformationEntropy(Wind);
+		infoGain.Entropy_S = Entropy_S;
+		System.out.println(infoGain.getInformationGain(GainTest));
+		/*System.out.println(TrainingInfo.size());
 		for ( String Training:TrainingInfo ) {
 			System.out.println(Training);
 		}
 		for ( String Head:Headers ) {
 			System.out.println(Head);
-		}
+		}*/
 	}
 
 }
